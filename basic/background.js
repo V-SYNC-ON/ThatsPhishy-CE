4c2f7a7eae59ffe7b9ec61e2f0dc7429f9007366
nijuyonkadesu <24ksriramv@gmail.com>
2023-09-09 08:22:25 +0530

    fix manifest, fix background script

--- basic/background.js
+++ basic/background.js
@@ -1,28 +1,35 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: 'SHUT'
+  });
+});
+
 const extensions = 'https://developer.chrome.com/docs/extensions'
 const webstore = 'https://developer.chrome.com/docs/webstore'
 
-chrome.runtime.onInstalled.addListener(async (tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
     if(tab.url.startsWith(extensions) || tab.url.startsWith(webstore)){
+
+        // each opened tab has a id
         const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        const nextState = prevState == 'ON' ? 'SHUT' : 'ON';
-    }
+        const nextState = prevState === 'ON' ? 'SHUT' : 'ON';
 
-    await chrome.action.setBadgeText({
-        tabId: tab.id,
-        text: nextState,
-    })
-
-    if (nextState === "ON") {
-        // Insert the CSS file when the user turns the extension on
-        await chrome.scripting.insertCSS({
-            files: ["focus-mode.css"],
-            target: { tabId: tab.id },
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text: nextState,
         });
-    } else if (nextState === "OFF") {
-        await chrome.scripting.removeCSS({
-            files: ["focus-mode.css"],
-            target: { tabId: tab.id },
-        })
+
+        if (nextState === 'ON') {
+            await chrome.scripting.insertCSS({
+                files: ['focus-mode.css'],
+                target: { tabId: tab.id }
+            });
+        } else if (nextState === 'SHUT') {
+            await chrome.scripting.removeCSS({
+                files: ['focus-mode.css'],
+                target: { tabId: tab.id }
+            });
+        }
     }
 
-})
+});
