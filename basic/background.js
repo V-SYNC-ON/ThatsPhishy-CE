d5407c2fed4e89164f8266d43c09f4978dc0201c
nijuyonkadesu <24ksriramv@gmail.com>
2023-09-09 07:22:29 +0530

    button with state

--- basic/background.js
+++ basic/background.js
@@ -1,5 +1,14 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-        text: "SHUT",
+const extensions = 'https://developer.chrome.com/docs/extensions'
+const webstore = 'https://developer.chrome.com/docs/webstore'
+
+chrome.runtime.onInstalled.addListener(async (tab) => {
+    if(tab.url.startsWith(extensions) || tab.url.startsWith(webstore)){
+        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+        const nextState = prevState == 'ON' ? 'SHUT' : 'ON';
+    }
+
+    await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: nextState,
     })
 })
