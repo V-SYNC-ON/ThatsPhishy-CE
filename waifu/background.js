3be42aec4dfbf42fccdfabc4ebc71a60bdd93336
nijuyonkadesu <24ksriramv@gmail.com>
2023-09-11 08:47:56 +0530

    setup a listener that calls API and updates scores to UI - onMessage listener - onConnect listener - sendMessage in popup.js (also the content js) - add working permission & host permissions

--- waifu/background.js
+++ waifu/background.js
@@ -4,3 +4,32 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+chrome.runtime.onConnect.addListener(port => {
+    port.onMessage.addListener(message => { console.log(message) })
+})
+
+chrome.runtime.onMessage.addListener(predict)
+
+function predict (req, sender, sendResponse) {
+    console.log("predict execution")
+    if(req.type === "predict"){
+        fetch('https://thats-phishy.onrender.com/predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ url: req.url })
+        })
+            .then(res => { 
+                return res.json()
+            }) 
+            .then(data => {
+                console.log(data); // Handle the data
+                sendResponse(data.prediction)
+            })
+            .catch(error => {
+                console.error('Error:', error); // Handle any errors
+            })
+        return true
+    }
+}
--- waifu/popup.js
+++ waifu/popup.js
@@ -1,12 +1,31 @@
 const result = document.getElementById("result")
 const button = document.querySelector("button")
 
-async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    result.textContent = tab.url
-    // return tab;
+result.textContent = '...'
+
+let queryOptions = { active: true, lastFocusedWindow: true };
+let [tab] = await chrome.tabs.query(queryOptions);
+
+function getCurrentTab() {
+    var req = {
+            type: "predict",
+            url: tab.url
+        }
+    console.log(tab.url)
+
+    chrome.runtime.sendMessage(
+        req, score => {
+            if(chrome.runtime.lastError){
+                console.log("Timeout")
+                setTimeout(getCurrentTab, 1000)
+            } else {
+                result.textContent = score
+            }
+        }
+    )
+    // ref: https://stackoverflow.com/questions/54181734/chrome-extension-message-passing-unchecked-runtime-lasterror-could-not-establi/54686484#54686484
 }
+
 
 button.addEventListener("click", getCurrentTab)
 
