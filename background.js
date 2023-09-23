3be20d2c0e7d72a23b6a397cb27f37de4a2de6fa
nijuyonkadesu <24ksriramv@gmail.com>
2023-09-23 14:27:31 +0530

    fix(api): add fallback value

--- background.js
+++ background.js
@@ -59,7 +59,15 @@ chrome.runtime.onConnect.addListener(port => {
 chrome.runtime.onMessage.addListener(predict)
 
 function predict (req, sender, sendResponse) {
+
+    fallbackValue = {
+        "description": "The website doesn't exist, either the URL is faulty or maybe the site is down",
+        "domain": "invalid domain",
+        "prediction": "-",
+        "status": "UNKNOWN"
+    }
     console.log("predict execution")
+
     if(req.type === "predict"){
         fetch('http://127.0.0.1:8080/predict', {
             method: 'POST',
@@ -72,19 +80,22 @@ function predict (req, sender, sendResponse) {
                 return res.json()
             }) 
             .then(data => {
-                console.log(data); // Handle the data
+                console.log("API Result: ", data); // Handle the data
+                if(data.error) sendResponse(fallbackValue)
+
                 sendResponse(data)
             })
             .catch(error => {
                 console.error('Error:', error); // Handle any errors
+                sendResponse(fallbackValue)
             })
         return true
     }
 }
 
 /* 
-    API Result Format:
 {
+    API Result Format:
     "description": "This website archlinux.org exhibits a concerning security profile, that could potentially impact your online security",
     "domain": "archlinux.org",
     "prediction": 0,
